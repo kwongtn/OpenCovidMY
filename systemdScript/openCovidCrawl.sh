6a7e540e608ba3328bd732a742c3e84531afe982
KwongTN <tungnan5636@gmail.com>
2021-01-20 12:42:16 +0800

    [Auto] 2021/01/20 12:00:20

--- systemdScript/openCovidCrawl.sh
+++ systemdScript/openCovidCrawl.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 dt=$(date '+%Y/%m/%d %H:%M:%S')
-cd /home/ubuntu/OpenCovidMY/crawler/src
+cd /home/ubuntu/OpenCovidMY/
 
 git pull
 git checkout auto 
 git pull
 
+sudo chown -Rc $UID .
+
+cd /home/ubuntu/OpenCovidMY/crawler/src
+
 npm install --include-dev 
 npm start
+
+cd /home/ubuntu/OpenCovidMY/
+sudo chown -Rc $UID .
 
 git add -A
 git commit -a -m "[Auto] $dt"
