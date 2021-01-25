6d798b65d1b8ba4f8d420bd1b68991e52e0c44b1
KwongTN <tungnan5636@gmail.com>
2021-01-25 12:46:13 +0800

    Updated automated script

--- systemdScript/openCovidCrawl.sh
+++ systemdScript/openCovidCrawl.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
+# Defines the datetime variable
 dt=$(date '+%Y/%m/%d %H:%M:%S')
+
+# Getting into the repository
 cd /home/ubuntu/OpenCovidMY/
 
+# Updating repository
 git pull
 git checkout auto 
 git pull
 
 #sudo chown -Rc $UID .
 
+# Start crawler sequence
 cd /home/ubuntu/OpenCovidMY/crawler/src
+npm install --include-dev 
+npm start
 
+# Start combiner sequence
+cd /home/ubuntu/OpenCovidMY/combiner/src
 npm install --include-dev 
 npm start
 
 cd /home/ubuntu/OpenCovidMY/
 #sudo chown -Rc $UID .
 
+# Add everything changed and commit
 git add -A
-sudo -u ubuntu git commit -a -m "[Auto] $dt"
+git commit -a -m "[Auto] $dt"
 
 #git remote add origin git@github.com:kwongtn/OpenCovidMY.git
 
+# Push using the "ubuntu" account
 sudo -u ubuntu git push 
