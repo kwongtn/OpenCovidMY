ffa7eebe7c72a5bc1ebfb02b7470b0e9c90d33e5
KwongTN <tungnan5636@gmail.com>
2021-01-30 08:36:48 +0800

    Stage stufff again

---
+++ combiner/models/thisProject.ts
@@ -0,0 +1,70 @@
+/**
+ * Model file describing our datatype used.
+ */
+
+export interface ProjectStateDataSingle {
+  state: string;
+  confirmed: number;
+  newCases: number;
+  importedNewCases: number;
+  death: number;
+  newDeath: number;
+  active: number;
+  changeActive: number;
+  activeFourteen: number;
+  changeActiveFourteen: number;
+}
+
+export interface ProjectStateData extends Array<ProjectStateDataSingle> {}
+
+export interface ProjectDistrictDataSingle {
+  state: string;
+  district: string;
+  cases: number;
+  newCases: number;
+  active: number;
+  changeActive: number;
+  activeFourteen: number;
+  changeActiveFourteen: number;
+}
+
+export interface ProjectDistrictData extends Array<ProjectDistrictDataSingle> {}
+
+export interface ProjectMukimDataSingle {
+  state: string;
+  district: string;
+  mukim: string;
+  cases: number;
+  newCases: number;
+  active: number;
+  changeActive: number;
+  activeFourteen: number;
+  changeActiveFourteen: number;
+}
+
+export interface ProjectMukimData extends Array<ProjectMukimDataSingle> {}
+
+export interface ProjectGeneralData {
+  positiveTested: number | null;
+  negativeTested: number | null;
+  pending: number | null;
+  timestamp: string;
+}
+
+export interface UnDeterminedClass {
+  url: string;
+  content: string;
+}
+
+export type DataClass =
+  | "state"
+  | "district"
+  | "mukim"
+  | "none"
+  | "cluster"
+  | "foreigner"
+  | "general";
+
+export interface DeterminedClass extends UnDeterminedClass {
+  type: DataClass;
+}
