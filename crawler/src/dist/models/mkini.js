93b4fdf3f2f68a0d4dd3fb0ab96d9cc47bda263b
KwongTN <tungnan5636@gmail.com>
2021-02-19 12:00:52 +0800

    [Auto] 2021/02/19 12:00:01

--- crawler/src/dist/models/mkini.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Model file describing data extracted from malaysia kini webpage.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=mkini.js.map
